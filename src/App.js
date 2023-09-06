d501c06089dba1fa15e2fc375dd0e7fc0e092b35
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-06 15:21:15 +0100

    sum

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 
+import React from 'react';
 import './App.css';
+import ChartComponent from './Chart';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-
+      <ChartComponent />
     </div>
   );
 }
+
+export default App;
 
